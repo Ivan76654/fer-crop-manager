d6fd32d1bf96cd8ec0040c7d4ac08b7d26fba252
Ivan Lovrić <ivan.lovric2101@gmail.com>
2024-06-11 21:56:44 +0200

    Add production environment configuration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  thingsboardConfig: {
+    hostUrl: 'http://http://161.53.19.19:45080',
+    tokenExpiresIn: 2.5 // hours
+  }
 };
