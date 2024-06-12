e1629bef8af4e3bb8ec28f7771d69706563eff76
Ivan Lovrić <ivan.lovric2101@gmail.com>
2024-06-12 14:46:53 +0200

    Add more routing, placeholder components and loading spinner component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { DevicesComponent } from './devices/devices.component';
+import { InfoComponent } from './info/info.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +17,13 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'devices',
+    component: DevicesComponent
+  },
+  {
+    path: 'user-info',
+    component: InfoComponent
   }
 ];
