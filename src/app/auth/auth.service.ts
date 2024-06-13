f6b9c4d248502f87c3db05608fd5325026fc3265
Ivan Lovrić <ivan.lovric2101@gmail.com>
2024-06-13 10:00:07 +0200

    Move user model to shared/data folder.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
-import { User } from './user.model';
+import { User } from '../shared/data/user.model';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
