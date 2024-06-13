723e6b92f303a5425d1a4be5d56ab4c29665b924
Ivan Lovrić <ivan.lovric2101@gmail.com>
2024-06-13 09:56:27 +0200

    Add web socket host URL.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   thingsboardConfig: {
     hostUrl: 'http://161.53.19.19:45080',
+    wsHostUrl: 'wss://161.53.19.19:45080/api/ws',
     tokenExpiresIn: 2.5 // hours
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   thingsboardConfig: {
     hostUrl: 'http://localhost:8080',
+    wsHostUrl: 'ws://127.0.0.1:8080/api/ws',
     tokenExpiresIn: 2.5 // hours
   }
 };
